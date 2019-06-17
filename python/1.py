50600347d0895af0fd60e8a07fb70ead910bbf33
Mesh <dexterrorez@gmail.com>
2019-06-17 22:15:55 +0300

    Calculator

--- python/1.py
+++ python/1.py
@@ -1,2 +1,22 @@
-print(5)
-print (10)
+
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+print(Back.WHITE)
+print(Fore.BLACK)
+print(Style.NORMAL)
+what = input('What do? (+/-): ')
+
+a = float(input ('Input first unit: '))
+b = float(input ('Input second unit: '))
+
+if what  == '+':
+    c = a + b
+    print('Result is: ' +  str(c))
+elif what == '-':
+    c = a - b
+    print('Result is: ' +  str(c))
+else:
+    print('You are moron')
